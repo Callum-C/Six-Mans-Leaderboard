0e0c0af96d96d8a6dc399670d5a74dd1fd85c34c
Callum-C <60474698+Callum-C@users.noreply.github.com>
2023-03-15 15:11:59 +0000

    Data displaying

--- scripts/database.js
+++ scripts/database.js
@@ -1,56 +1,73 @@
 
-import headers from '../config.json';
+async function callAPI() {
+  document.getElementById("demo").innerHTML = "Loading...";
 
-module.exports = {
-  callAPI: async () => {
-    document.getElementById("demo").innerHTML = "Loading...";
+  let table = createTable();
+
+  const response = await fetch(
+    'https://chinney98.api.stdlib.com/six-mans-api/'
+  );
+  const data = await response.json();
+
+  for (const player of data) {
+    table = addPlayer(table, player);
+  }
+
+  document.getElementById("mainContent").appendChild(table);
+  //document.getElementById("demo").innerHTML = `${JSON.stringify(data)}`;
+}
   
-    let table = createTable();
+/**
+ * Create Table with Header Row.
+ *  
+ * @returns {HTMLTableElement}
+ */
+function createTable () {
+  const headers = [
+    "Username", "Matches Won", "Win Streak", "Match Diff", "Matches Played",
+    "Win Percentage", "Games Won", "Game Diff", "Games Played"
+  ];
+
+  let table = document.createElement('table');
+  const tr = document.createElement('tr');
+
+  for (let i=0; i < headers.length; i++) {
+    const th = document.createElement('th');
+    const text = document.createTextNode(headers[i]);
+    th.appendChild(text);
+    tr.appendChild(th);
+  }
+
+  table.appendChild(tr);
+  return table;
+}
   
-    const response = await fetch(
-      'https://chinney98.api.stdlib.com/six-mans-api/'
-    );
-    const data = await response.json();
-  
-    for (const player of data) {
-      table.insertRow();
-  
+/**
+  * Add a singular player to the leaderboard table.
+  * 
+  * @param {HTMLTableElement} table
+  * @param {Object} player
+  * @returns {HTMLTableElement} table
+  */
+function addPlayer (table, player) {
+
+  const tr = document.createElement('tr');
+
+  // Show user name first
+  const td = document.createElement('td');
+  const text = document.createTextNode(player.username);
+  td.appendChild(text);
+  tr.appendChild(td);
+
+  for (const property in player) {
+    if (property != "username") {
+      const cell = document.createElement('td');
+      const txt = document.createTextNode(player[property]);
+      cell.appendChild(txt);
+      tr.appendChild(cell);
     }
-  
-    document.getElementById("mainContent").appendChild(table);
-    //document.getElementById("demo").innerHTML = `${JSON.stringify(data)}`;
-  },
-  
-  /**
-   * Create Table with Header Row.
-   * 
-   * @returns Leaderboard Table
-   */
-  createTable: async () => {
-    
-  
-    let table = document.createElement('table');
-    let tr = document.createElement('tr');
-  
-    for (let i=0; i < headers.length; i++) {
-      const th = document.createElement('th');
-      const text = document.createTextNode(headers[i]);
-      th.appendChild(text);
-      tr.appendChild(th);
-    }
-  
-    table.appendChild(tr);
-  
-    return table;
-  },
-  
-  /**
-   * Add a singular player to the leaderboard table.
-   * 
-   */
-  addPlayer: () => {
-  
-  },
+  }
 
-};
-
+  table.appendChild(tr);
+  return table;
+}
