46865619de8efc12abb1d8aeab380a8e6acd359f
Callum-C <60474698+Callum-C@users.noreply.github.com>
2023-09-19 13:22:28 +0100

    Before Refactor

--- scripts/database.js
+++ scripts/database.js
@@ -81,23 +81,6 @@ function createTable () {
   
   return table;
 }
-
-/**
- * Get parameter from URL.
- * 
- * @param {String} sParam Search Parameter
- * @returns 
- */
-function getURLParam(sParam) {
-  const sPageURL = window.location.search.substring(1);
-  const sURLVariables = sPageURL.split('&');
-  for (let i = 0; i < sURLVariables.length; i++) {
-    const sParameterName = sURLVariables[i].split('=');
-    if (sParameterName[0] === sParam) {
-      return sParameterName[1];
-    }
-  }
-}
   
 /**
   * Add a singular player to the leaderboard table.
--- scripts/main.js
+++ scripts/main.js
@@ -46,3 +46,20 @@ function getThemePreference(nested=false) {
     theme.setAttribute('href', pathToTheme);
   }
 };
+
+/**
+ * Get parameter from URL.
+ * 
+ * @param {String} sParam Search Parameter
+ * @returns 
+ */
+function getURLParam(sParam) {
+  const sPageURL = window.location.search.substring(1);
+  const sURLVariables = sPageURL.split('&');
+  for (let i = 0; i < sURLVariables.length; i++) {
+    const sParameterName = sURLVariables[i].split('=');
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1];
+    }
+  }
+};
