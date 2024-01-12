d422466cde140b21becbf33955fb2968dfd37778
Callum-C <60474698+Callum-C@users.noreply.github.com>
2024-01-12 15:55:29 +0000

    Update API Link

--- scripts/database.js
+++ scripts/database.js
@@ -20,7 +20,7 @@ async function callAPI () {
   guildID = guildID ? guildID : "349293115225407488";
   
   const response = await fetch(
-    `https://r5dvsot262.execute-api.eu-west-1.amazonaws.com/default/SixMansLeaderboardAPI` + 
+    `https://orc8aw0hui.execute-api.eu-west-1.amazonaws.com/Initial/stats/` + 
     `?guildID=${guildID}`
   );
   const data = await response.json();
